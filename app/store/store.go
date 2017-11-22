0a5a759a09d788bd6886fd76bdf1bed6a7f58c12
Dmitry Verkhoturov <paskal.07@gmail.com>
2017-11-22 19:52:44 +0300

    move parse functions away from store package

--- app/store/store.go
+++ app/store/store.go
@@ -1,58 +1,25 @@
 package store
 
 import (
-	"log"
-	"regexp"
 	"time"
-)
 
-// LogEntry contains meaningful data extracted from single log line
-type LogEntry struct {
-	SourceIP        string
-	FileName        string
-	DestinationNode string
-	AnswerTime      time.Duration
-	Date            time.Time
-}
+	"github.com/umputun/rlb-stats/app/parse"
+)
 
 // Candle contains one minute candle from log entries for that period
 type Candle struct {
-	UniqIPsNum      string
+	Volume          int
+	UniqIPsNum      int
 	FileName        string
 	DestinationNode string
+	MinAnswerTime   time.Duration
 	MeanAnswerTime  time.Duration
+	MaxAnswerTime   time.Duration
 	MinuteStart     time.Time
 }
 
-// ParseLine parse log line into LogEntry
-func ParseLine(line string, regEx *regexp.Regexp) (entry LogEntry, err error) {
-	result := regEx.FindStringSubmatch(line)
-	n := regEx.SubexpNames()
-	for i, m := range result {
-		switch n[i] {
-		case "": // Do nothing, first result is full string
-		case "SourceIP":
-			entry.SourceIP = m
-		case "FileName":
-			entry.FileName = m
-		case "DestinationNode":
-			entry.DestinationNode = m
-		case "AnswerTime":
-			entry.AnswerTime, _ = time.ParseDuration(m)
-		case "Date":
-			entry.Date, _ = time.Parse("2006/01/02 15:04:05", m)
-			// TODO: validate regex to make sure it doesn't contain wrong fields and contain right fields
-		default:
-			log.Fatalf("[ERROR] unknown field '%s'", n[i])
-		}
-	}
-	return
-}
-
-// TODO: write entries to boltDB
-
 // Engine defines interface to save log entries and load candels
 type Engine interface {
-	Save(msg *LogEntry) (err error)
-	Load(periodStart, periodEnd time.Time) (resutl *Candle, err error)
+	Save(msg *parse.LogEntry) (err error)
+	Load(periodStart, periodEnd time.Time) (result []Candle, err error)
 }
