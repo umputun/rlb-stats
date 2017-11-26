6a6cacbbc9dd6c5d612a892fd8654c2cedf03861
Dmitry Verkhoturov <paskal.07@gmail.com>
2017-11-26 15:55:02 +0300

    fix bolt test

--- app/store/bolt_test.go
+++ app/store/bolt_test.go
@@ -13,7 +13,13 @@ func TestSaveAndLoadLogEntryBolt(t *testing.T) {
 	s, err := NewBolt("/tmp/test.bd", time.Minute)
 	assert.Nil(t, err, "engine created")
 
-	logEntry := parse.LogEntry{SourceIP: "127.0.0.1", FileName: "rtfiles/rt_podcast561.mp3", DestinationNode: "n6.radio-t.com", AnswerTime: time.Second, Date: time.Now()}
+	logEntry := parse.LogEntry{
+		SourceIP:        "127.0.0.1",
+		FileName:        "rtfiles/rt_podcast561.mp3",
+		DestinationNode: "n6.radio-t.com",
+		AnswerTime:      time.Second,
+		Date:            time.Now().Round(0), // Round to drop monotonic clock, https://tip.golang.org/pkg/time/#hdr-Monotonic_Clocks
+	}
 	assert.Nil(t, s.Save(&logEntry), "saved fine")
 	savedEntry, err := s.loadLogEntry(time.Now(), time.Now().Add(time.Second))
 	assert.Nil(t, err, "key found")
