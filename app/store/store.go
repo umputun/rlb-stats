a3deb8c1058e5b5aae6e3aac2c1a01a142632f8d
Dmitry Verkhoturov <paskal.07@gmail.com>
2017-12-03 15:26:47 +0300

    rewrite store structure, add functions to work with it

--- app/store/store.go
+++ app/store/store.go
@@ -6,20 +6,73 @@ import (
 	"github.com/umputun/rlb-stats/app/parse"
 )
 
-// Candle contains one minute candle from log entries for that period
+// Candle contain one minute candle from log entries for that period
 type Candle struct {
-	Volume          int
-	UniqIPsNum      int
-	FileName        string
-	DestinationNode string
-	MinAnswerTime   time.Duration
-	MeanAnswerTime  time.Duration
-	MaxAnswerTime   time.Duration
-	MinuteStart     time.Time
+	Nodes       map[string]Info
+	StartMinute time.Time
+}
+
+// Info contain single node download statistics
+type Info struct {
+	Volume         int
+	MinAnswerTime  time.Duration
+	MeanAnswerTime time.Duration
+	MaxAnswerTime  time.Duration
+	Files          map[string]int
 }
 
 // Engine defines interface to save log entries and load candels
 type Engine interface {
 	Save(msg parse.LogEntry) (err error)
 	Load(periodStart, periodEnd time.Time) (result []Candle, err error)
+}
+
+func infoFromLog(l parse.LogEntry) Info {
+	return Info{
+		Volume:         1,
+		MinAnswerTime:  l.AnswerTime,
+		MeanAnswerTime: l.AnswerTime,
+		MaxAnswerTime:  l.AnswerTime,
+		Files:          map[string]int{l.FileName: 1},
+	}
+}
+
+func (n Info) appendLog(l parse.LogEntry) Info {
+	count, fileInNode := n.Files[l.FileName]
+	if fileInNode {
+		n.Files[l.FileName] = count + 1
+	} else {
+		n.Files[l.FileName] = 1
+	}
+	if n.MinAnswerTime > l.AnswerTime {
+		n.MinAnswerTime = l.AnswerTime
+	}
+	if n.MaxAnswerTime < l.AnswerTime {
+		n.MaxAnswerTime = l.AnswerTime
+	}
+	// TODO get average of durations
+	//n.MeanAnswerTime = (n.MeanAnswerTime * n.Volume) + n.MeanAnswerTime) / (n.Volume + 1)
+	n.Volume++
+	return n
+}
+
+func createCandle(l parse.LogEntry) (c Candle) {
+	node := infoFromLog(l)
+	c.Nodes = map[string]Info{
+		l.DestinationNode: node,
+		"all":             node,
+	}
+	return
+}
+
+func appendToCandle(c Candle, l parse.LogEntry) Candle {
+	node, nodeInCandle := c.Nodes[l.DestinationNode]
+	if nodeInCandle {
+		node = node.appendLog(l)
+	} else {
+		node = infoFromLog(l)
+	}
+	c.Nodes[l.DestinationNode] = node
+	c.Nodes["all"] = c.Nodes["all"].appendLog(l)
+	return c
 }
