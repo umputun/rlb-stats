33b323ee8311b6692310ffebd46e39fb352f6d46
Dmitry Verkhoturov <paskal.07@gmail.com>
2017-12-21 15:53:18 +0300

    initial main stub

--- app/main.go
+++ app/main.go
@@ -2,24 +2,50 @@ package main
 
 import (
 	"log"
+
+	"os"
+
+	"github.com/jessevdk/go-flags"
+
+	"github.com/umputun/rlb-stats/app/parse"
+	"github.com/umputun/rlb-stats/app/store"
 )
 
-//const defaultRegEx = `^(?P<Date>.+) - (?:.+) - (?P<FileName>.+) - (?P<SourceIP>.+) - (?:.+) - (?P<AnswerTime>.+) - https?://(?P<DestinationNode>.+?)/.+$`
-//const dbFilename = "/tmp/rlb-stats.bd"
-
-// TODO: command-line parameters \ env
-// regexp, dbFilename, containerName, debug
-
-func main() {
-	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
-	//store := getEngine(dbFilename)
-	//parser := parгse.New(defaultRegEx)
+var opts struct {
+	RegEx         string `long:"regexp" env:"REGEXP" description:"log line regexp" default:"^(?P<Date>.+) - (?:.+) - (?P<FileName>.+) - (?P<SourceIP>.+) - (?:.+) - (?P<AnswerTime>.+) - https?://(?P<DestinationNode>.+?)/.+$"`
+	ContainerName string `long:"container_name" env:"CONTAINER_NAME" default:"" description:"container name"`
+	BoltDB        string `long:"bolt" env:"BOLT_FILE" default:"/tmp/rlb-stats.bd" description:"boltdb file"`
+	Dbg           bool   `long:"dbg" description:"debug mode"`
 }
 
-//func getEngine(boltFile string) store.Engine {
-//	store, err := store.NewBolt(boltFile)
-//	if err != nil {
-//		log.Fatalf("[ERROR] can't open db, %v", err)
-//	}
-//	return store
-//}
+var revision string
+
+func main() {
+	if _, err := flags.Parse(&opts); err != nil {
+		os.Exit(1)
+	}
+
+	log.SetFlags(log.Ldate | log.Ltime)
+	if opts.Dbg {
+		log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
+	}
+	log.Printf("rlb-stats %s", revision)
+	getEngine(opts.BoltDB)
+	getParser(opts.RegEx)
+}
+
+func getEngine(boltFile string) store.Engine {
+	storage, err := store.NewBolt(boltFile)
+	if err != nil {
+		log.Fatalf("[ERROR] can't open db, %v", err)
+	}
+	return storage
+}
+
+func getParser(regEx string) *parse.Parser {
+	parser, err := parse.New(regEx)
+	if err != nil {
+		log.Fatalf("[ERROR] can't validate regex, %v", err)
+	}
+	return parser
+}
