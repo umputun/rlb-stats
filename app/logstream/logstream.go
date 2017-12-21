9cafb0cd8e80e925fa3528ca35353f7b66dc0feb
Dmitry Verkhoturov <paskal.07@gmail.com>
2017-12-21 12:18:58 +0300

    logstream initial implementation

---
+++ app/logstream/logstream.go
@@ -0,0 +1,61 @@
+package logstream
+
+import (
+	"io"
+	"log"
+	"time"
+
+	"bytes"
+
+	"github.com/fsouza/go-dockerclient"
+)
+
+// LogStreamer connects and activates container's log stream with io.Writer
+type LogStreamer struct {
+	DockerClient *docker.Client
+	ContainerID  string
+	LogWriter    io.Writer
+	ErrWriter    io.Writer
+}
+
+// LineExtractor have buffer to store bytes before \n happen and channel to return complete line
+type LineExtractor struct {
+	ch  chan string
+	buf []byte
+}
+
+// NewLineExtractor create LineExtractor
+func NewLineExtractor() *LineExtractor {
+	return &LineExtractor{ch: make(chan string)}
+}
+
+// Go activates streamer
+func (l *LogStreamer) Go() {
+	log.Printf("[INFO] start log streamer for %s", l.ContainerID)
+	go func() {
+		logOpts := docker.LogsOptions{
+			Container:         l.ContainerID,
+			OutputStream:      l.LogWriter, // logs writer for stdout
+			ErrorStream:       l.ErrWriter, // err writer for stderr
+			Tail:              "10",
+			Follow:            true,
+			Stdout:            true,
+			Stderr:            true,
+			InactivityTimeout: time.Hour * 10000,
+		}
+		err := l.DockerClient.Logs(logOpts) // this is blocking call. Will run until container up and will publish to streams
+		log.Printf("[INFO] stream from %s terminated, %v", l.ContainerID, err)
+	}()
+}
+
+// Write complete strings into channel
+func (le *LineExtractor) Write(p []byte) (n int, err error) {
+	le.buf = append(le.buf, p...)
+
+	if n := bytes.IndexByte(le.buf, '\n'); n >= 0 {
+		line := string(le.buf[:n])
+		le.ch <- string(line)
+		le.buf = bytes.TrimLeft(le.buf, line+"\n")
+	}
+	return len(p), nil
+}
---
+++ app/logstream/logstream_test.go
@@ -0,0 +1,39 @@
+package logstream
+
+import (
+	"testing"
+	"time"
+
+	"fmt"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/umputun/rlb-stats/app/parse"
+)
+
+func TestLogExtraction(t *testing.T) {
+	regEx := `^(?P<Date>.+) - (?:.+) - (?P<FileName>.+) - (?P<SourceIP>.+) - (?:.+) - (?P<AnswerTime>.+) - https?://(?P<DestinationNode>.+?)/.+$`
+	parser, _ := parse.New(regEx)
+	lineExtractor := NewLineExtractor()
+
+	lineExtractor.Write([]byte(fmt.Sprint("2017/09/17 12:54:54.095329 - GET - /api/v1/jump/fil")))
+	lineExtractor.Write([]byte(fmt.Sprint("es?url=/rtfiles/rt_podcast561.mp3 - 213.87.120.120 - 302 (70) - 710.679µs - http://n6.radio-t.com/rtfiles/rt_podcast561.mp3\n")))
+	lineExtractor.Write([]byte(fmt.Sprint("2017/09/17 12:54:54.095329 - GET - /api/v1/jump/files?url=/rtfiles/rt_podcast561.mp3 - 213.87.120.120 - 302 (70) - 710.679µs - http://n6.radio-t.com/rtfiles/rt_podcast561.mp3\n2017/09/17 12:54:54.095329 - GET - /api/v1/jump/files?url=/rtfiles/rt_podcast561.mp3 - 213.87.120.120 - 302 (70) - 710.679µs - http://n6.radio-t.com/rtfiles/rt_podcast561.mp3")))
+
+	var entries []parse.LogEntry
+	for line := range lineExtractor.ch {
+		entry, err := parser.Do(line)
+		assert.Nil(t, err, "single line parsed")
+		entries = append(entries, entry)
+	}
+
+	entryParsed := parse.LogEntry{
+		SourceIP:        "213.87.120.120",
+		FileName:        "/api/v1/jump/files?url=/rtfiles/rt_podcast561.mp3",
+		DestinationNode: "n6.radio-t.com",
+		AnswerTime:      time.Nanosecond * 710679,
+		Date:            time.Date(2017, 9, 17, 12, 54, 54, 95329000, time.Time{}.Location()),
+	}
+	entriesParsed := []parse.LogEntry{entryParsed, entryParsed, entryParsed}
+	assert.Equal(t, entriesParsed, entries, "entries parsed")
+
+}
