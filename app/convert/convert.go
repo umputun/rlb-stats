31647706dd9255fa569283538082ab4f762f8439
Dmitry Verkhoturov <paskal.07@gmail.com>
2017-12-22 03:29:58 +0300

    working convert module version

--- app/convert/convert.go
+++ app/convert/convert.go
@@ -4,46 +4,42 @@ import (
 	"fmt"
 	"time"
 
-	"sort"
-
 	"github.com/umputun/rlb-stats/app/candle"
 	"github.com/umputun/rlb-stats/app/parse"
 )
 
-// Do convert LogEntries into Candles,
-// dropping duplicate IP-filename pairs each minute
-func Do(entries []parse.LogEntry) []candle.Candle {
-	c := make(map[time.Time]candle.Candle)
-	deduplicate := make(map[string]struct{})
-	for _, entry := range entries {
-		// drop seconds and nanoseconds from log date
-		entry.Date = time.Date(
-			entry.Date.Year(),
-			entry.Date.Month(),
-			entry.Date.Day(),
-			entry.Date.Hour(),
-			entry.Date.Minute(),
-			0,
-			0,
-			entry.Date.Location())
-		_, duplicate := deduplicate[fmt.Sprintf("%d-%s-%s", entry.Date.Unix(), entry.FileName, entry.SourceIP)]
-		if !duplicate {
-			newCandle, exists := c[entry.Date]
-			if !exists {
-				newCandle = candle.NewCandle()
+// entries contain previous entries
+var entries []parse.LogEntry
+
+// Submit store LogEntry and return Candle when minute change
+func Submit(newEntry parse.LogEntry) (candle.Candle, bool) {
+	minuteCandle := candle.Candle{}
+	ok := false
+	// drop seconds and nanoseconds from log date
+	newEntry.Date = time.Date(
+		newEntry.Date.Year(),
+		newEntry.Date.Month(),
+		newEntry.Date.Day(),
+		newEntry.Date.Hour(),
+		newEntry.Date.Minute(),
+		0,
+		0,
+		newEntry.Date.Location())
+
+	if len(entries) > 0 && !newEntry.Date.Equal(entries[len(entries)-1].Date) { // if there are existing entries and date changed
+		minuteCandle = candle.NewCandle()           // then all previous entries have same date precise to the minute and will be written to single candle
+		var deduplicate = make(map[string]struct{}) // deduplicate store ip-file map
+		for _, entry := range entries {
+			_, duplicate := deduplicate[fmt.Sprintf("%s-%s", entry.FileName, entry.SourceIP)]
+			if !duplicate {
+				minuteCandle.Update(entry)
+				deduplicate[fmt.Sprintf("%s-%s", entry.FileName, entry.SourceIP)] = struct{}{}
 			}
-			newCandle.Update(entry)
-			c[entry.Date] = newCandle
-			deduplicate[fmt.Sprintf("%d-%s-%s", entry.Date.Unix(), entry.FileName, entry.SourceIP)] = struct{}{}
 		}
+		ok = true                    // candle is ready to be written
+		entries = []parse.LogEntry{} // clean written entries
 	}
+	entries = append(entries, newEntry)
 
-	var candles []candle.Candle
-
-	for _, value := range c {
-		candles = append(candles, value)
-	}
-	// sort the slice to make sure we return test data in same order
-	sort.Slice(candles, func(i, j int) bool { return candles[i].StartMinute.Before(candles[j].StartMinute) })
-	return candles
+	return minuteCandle, ok
 }
--- app/convert/convert_test.go
+++ app/convert/convert_test.go
@@ -10,51 +10,71 @@ import (
 )
 
 func TestLogConversion(t *testing.T) {
-	logEntries := []parse.LogEntry{
-		{
-			SourceIP:        "127.0.0.1", // access to first file
-			FileName:        "/rtfiles/rt_podcast561.mp3",
-			DestinationNode: "n6.radio-t.com",
-			AnswerTime:      time.Second,
-			Date:            time.Time{},
-		}, {
-			SourceIP:        "127.0.0.1", // access to second file
-			FileName:        "/rtfiles/rt_podcast562.mp3",
-			DestinationNode: "n6.radio-t.com",
-			AnswerTime:      time.Second,
-			Date:            time.Time{},
-		}, {
-			SourceIP:        "127.0.0.1", // access to first file, other node
-			FileName:        "/rtfiles/rt_podcast561.mp3",
-			DestinationNode: "n7.radio-t.com",
-			AnswerTime:      time.Second,
-			Date:            time.Time{},
-		}, {
-			SourceIP:        "127.0.0.1", // access to first file, other minute
-			FileName:        "/rtfiles/rt_podcast561.mp3",
-			DestinationNode: "n7.radio-t.com",
-			AnswerTime:      time.Second,
-			Date:            time.Time{}.Add(time.Minute),
-		},
+	logEntry1 := parse.LogEntry{
+		SourceIP:        "127.0.0.1", // access to first file
+		FileName:        "/rtfiles/rt_podcast561.mp3",
+		DestinationNode: "n6.radio-t.com",
+		AnswerTime:      time.Second,
+		Date:            time.Time{},
+	}
+	logEntry2 := parse.LogEntry{
+		SourceIP:        "127.0.0.1", // access to second file
+		FileName:        "/rtfiles/rt_podcast562.mp3",
+		DestinationNode: "n6.radio-t.com",
+		AnswerTime:      time.Second,
+		Date:            time.Time{},
 	}
 
-	candles := Do(logEntries)
-	testCandles := []candle.Candle{
-		{
-			Nodes: map[string]candle.Info{
-				"n6.radio-t.com": {2, time.Second, time.Second, time.Second, map[string]int{"/rtfiles/rt_podcast561.mp3": 1, "/rtfiles/rt_podcast562.mp3": 1}},
-				"all":            {2, time.Second, time.Second, time.Second, map[string]int{"/rtfiles/rt_podcast561.mp3": 1, "/rtfiles/rt_podcast562.mp3": 1}},
-			},
-			StartMinute: time.Time{},
-		}, {
-			Nodes: map[string]candle.Info{
-				"n7.radio-t.com": {1, time.Second, time.Second, time.Second, map[string]int{"/rtfiles/rt_podcast561.mp3": 1}},
-				"all":            {1, time.Second, time.Second, time.Second, map[string]int{"/rtfiles/rt_podcast561.mp3": 1}},
-			},
-			StartMinute: time.Time{}.Add(time.Minute),
+	logEntry3 := parse.LogEntry{
+		SourceIP:        "127.0.0.1", // access to first file, other node
+		FileName:        "/rtfiles/rt_podcast561.mp3",
+		DestinationNode: "n7.radio-t.com",
+		AnswerTime:      time.Second,
+		Date:            time.Time{},
+	}
+	logEntry4 := parse.LogEntry{
+		SourceIP:        "127.0.0.1", // access to first file, other minute
+		FileName:        "/rtfiles/rt_podcast561.mp3",
+		DestinationNode: "n7.radio-t.com",
+		AnswerTime:      time.Second,
+		Date:            time.Time{}.Add(time.Minute),
+	}
+	logEntry5 := parse.LogEntry{
+		SourceIP:        "127.0.0.1", // access in third minute, will not be flushed into resultCandle
+		FileName:        "/rtfiles/rt_podcast561.mp3",
+		DestinationNode: "n7.radio-t.com",
+		AnswerTime:      time.Second,
+		Date:            time.Time{}.Add(time.Minute * 2),
+	}
+	testCandle1 := candle.Candle{ // from first 3 entries
+		Nodes: map[string]candle.Info{
+			"n6.radio-t.com": {Volume: 2, MinAnswerTime: time.Second, MeanAnswerTime: time.Second, MaxAnswerTime: time.Second, Files: map[string]int{"/rtfiles/rt_podcast561.mp3": 1, "/rtfiles/rt_podcast562.mp3": 1}},
+			"all":            {Volume: 2, MinAnswerTime: time.Second, MeanAnswerTime: time.Second, MaxAnswerTime: time.Second, Files: map[string]int{"/rtfiles/rt_podcast561.mp3": 1, "/rtfiles/rt_podcast562.mp3": 1}},
 		},
+		StartMinute: time.Time{},
+	}
+	testCandle2 := candle.Candle{ // from 4th entry
+		Nodes: map[string]candle.Info{
+			"n7.radio-t.com": {Volume: 1, MinAnswerTime: time.Second, MeanAnswerTime: time.Second, MaxAnswerTime: time.Second, Files: map[string]int{"/rtfiles/rt_podcast561.mp3": 1}},
+			"all":            {Volume: 1, MinAnswerTime: time.Second, MeanAnswerTime: time.Second, MaxAnswerTime: time.Second, Files: map[string]int{"/rtfiles/rt_podcast561.mp3": 1}},
+		},
+		StartMinute: time.Time{}.Add(time.Minute),
 	}
 
-	assert.EqualValues(t, candles, testCandles, "matches loaded msg")
+	resultCandle, ok := Submit(logEntry1)
+	assert.EqualValues(t, candle.Candle{}, resultCandle, "empty resultCandle in output")
+	assert.False(t, ok, "resultCandle not dumped")
+	resultCandle, ok = Submit(logEntry2)
+	assert.EqualValues(t, candle.Candle{}, resultCandle, "empty resultCandle in output")
+	assert.False(t, ok, "resultCandle not dumped")
+	resultCandle, ok = Submit(logEntry3)
+	assert.EqualValues(t, candle.Candle{}, resultCandle, "empty resultCandle in output")
+	assert.False(t, ok, "resultCandle not dumped")
+	resultCandle, ok = Submit(logEntry4)
+	assert.EqualValues(t, testCandle1, resultCandle, "resultCandle from first 3 records in output")
+	assert.True(t, ok, "resultCandle dumped")
+	resultCandle, ok = Submit(logEntry5)
+	assert.EqualValues(t, testCandle2, resultCandle, "resultCandle from 4th record in output")
+	assert.True(t, ok, "resultCandle dumped")
 
 }
