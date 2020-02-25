6e0e7b71c287f84242ca083715d838fc5ea1928a
Ksinia <xiatian@mail.ru>
2020-02-25 01:28:52 +0100

    Multiple review fixes

--- webapp/index.js
+++ webapp/index.js
@@ -1,63 +1,61 @@
+loadAndDraw();
+
 async function loadData(args) {
-  const url = `http://127.0.0.1:8080/api/candle?from=${args.from.toISOString()}&to=${args.to.toISOString()}&aggregate=${
-    args.aggregate
-  }m`;
+  const url = `/api/candle?from=${args.from.toISOString()}&to=${args.to.toISOString()}`;
+  const errorParagraph = document.getElementById("error");
+  errorParagraph.innerHTML = "";
   try {
     const response = await fetch(url);
-    const jsonData = await response.json();
-    return jsonData;
   } catch (error) {
-    console.log(error);
+    errorParagraph.innerHTML = `Error retrieving data for graphs: ${error.message}`;
+    return [];
   }
+  if (!response.ok) {
+    errorParagraph.innerHTML = `Error retrieving data for graphs: ${response.status} ${response.statusText}`;
+    return [];
+  }
+  const jsonData = await response.json();
+  return jsonData;
 }
 
 function transformToFiles(jsonData) {
+  if (jsonData.length === 0) {
+    return { files: [], nodes: [] };
+  }
   const filesData = jsonData.reduce(
     (acc, element) => {
-      acc.files[0].data.push([
-        new Date(element.StartMinute).getTime(),
-        element.Nodes.all.Volume
-      ]);
+      const entryDate = new Date(element.StartMinute).getTime();
+
+      // all files volume to given minute
+      acc.files[0].data.push([entryDate, element.Nodes.all.Volume]);
+
+      // per-file stats for given minute
       Object.keys(element.Nodes.all.Files).forEach(file => {
         if (!acc.files.find(el => el.file === file)) {
           acc.files.push({
             file: file,
-            data: [
-              [
-                new Date(element.StartMinute).getTime(),
-                element.Nodes.all.Files[file]
-              ]
-            ]
+            data: [[entryDate, element.Nodes.all.Files[file]]]
           });
         } else {
           acc.files.forEach(el => {
             if (el.file === file) {
-              el.data.push([
-                new Date(element.StartMinute).getTime(),
-                element.Nodes.all.Files[file]
-              ]);
+              el.data.push([entryDate, element.Nodes.all.Files[file]]);
             }
           });
         }
       });
+
+      // per-node stats for given minute
       Object.keys(element.Nodes).forEach(node => {
         if (!acc.nodes.find(el => el.node === node)) {
           acc.nodes.push({
             node: node,
-            data: [
-              [
-                new Date(element.StartMinute).getTime(),
-                element.Nodes[node].Volume
-              ]
-            ]
+            data: [[entryDate, element.Nodes[node].Volume]]
           });
         } else {
           acc.nodes.forEach(el => {
             if (el.node === node) {
-              el.data.push([
-                new Date(element.StartMinute).getTime(),
-                element.Nodes[node].Volume
-              ]);
+              el.data.push([entryDate, element.Nodes[node].Volume]);
             }
           });
         }
@@ -67,28 +65,10 @@ function transformToFiles(jsonData) {
     },
     { files: [{ file: "all", data: [] }], nodes: [] }
   );
-  const sortedFilesData = filesData.files.sort((a, b) => {
-    return (
-      b.data.reduce((acc, element) => {
-        return (acc += element[1]);
-      }, 0) -
-      a.data.reduce((acc, element) => {
-        return (acc += element[1]);
-      }, 0)
-    );
-  });
 
+  const sortedFilesData = sortFilesOrNodes(filesData.files);
+  const sortedNodesData = sortFilesOrNodes(filesData.nodes);
   // choose only top 10 files or nodes and total qty
-  const sortedNodesData = filesData.nodes.sort((a, b) => {
-    return (
-      b.data.reduce((acc, element) => {
-        return (acc += element[1]);
-      }, 0) -
-      a.data.reduce((acc, element) => {
-        return (acc += element[1]);
-      }, 0)
-    );
-  });
   return {
     files: sortedFilesData.slice(0, 11),
     nodes: sortedNodesData.slice(0, 11)
@@ -104,18 +84,21 @@ const dateTimeOptions = {
   minute: "2-digit"
 };
 
-// default charts are for last 10 days with 144 minutes aggregate
-async function loadAndDraw({
-  from = new Date(new Date().setDate(new Date().getDate() - 10)),
-  to = new Date(),
-  aggregate = 144
-} = {}) {
-  const jsonData = await loadData({ to, from, aggregate });
+// default charts are for last 24 hours
+async function loadAndDraw(minutes = 24 * 60) {
+  document.title = `rlb-stats: data for the last ${getReadableDuration(
+    minutes
+  )}`;
+  const args = {
+    from: new Date(new Date().setMinutes(new Date().getMinutes() - minutes)),
+    to: new Date()
+  };
+  const jsonData = await loadData(args);
   const data = transformToFiles(jsonData);
   drawChart(
     data.files,
     "top-files",
-    `Top 10 files downloaded from ${from.toLocaleDateString(
+    `Top 10 files downloaded from ${args.from.toLocaleDateString(
       [],
       dateTimeOptions
     )}`
@@ -123,42 +106,13 @@ async function loadAndDraw({
   drawChart(
     data.nodes,
     "top-nodes",
-    `Top 10 nodes from ${from.toLocaleDateString([], dateTimeOptions)}`
+    `Top 10 nodes from ${args.from.toLocaleDateString([], dateTimeOptions)}`
   );
 }
-
-anychart.onDocumentLoad(() => {
-  loadAndDraw();
-});
-
-const buttons = document.getElementsByClassName("period-button");
-for (let button of buttons) {
-  button.onclick = event => {
-    let args = {};
-    if (event.target.id === "1hour") {
-      args = {
-        from: new Date(new Date().setHours(new Date().getHours() - 1)),
-        aggregate: 1
-      };
-    } else if (event.target.id === "12hours") {
-      args = {
-        from: new Date(new Date().setHours(new Date().getHours() - 12)),
-        aggregate: 1
-      };
-    } else if (event.target.id === "24hours") {
-      args = {
-        from: new Date(new Date().setDate(new Date().getDate() - 1)),
-        aggregate: 1
-      };
-    } else if (event.target.id === "10days") {
-      args = {
-        from: new Date(new Date().setDate(new Date().getDate() - 10)),
-        aggregate: 144
-      };
-    }
-    loadAndDraw(args);
-  };
-}
+const buttons = document.getElementById("period-buttons");
+buttons.onclick = event => {
+  loadAndDraw(parseInt(event.target.dataset.minutes));
+};
 
 function drawChart(data, container, title) {
   //clear the container
@@ -185,8 +139,6 @@ function drawChart(data, container, title) {
   // set the maximum height and width of the legend
   chart.legend().height("220px");
   chart.height("500px");
-  const margin = chart.legend().margin();
-  margin.top(10);
 
   // enable html for the legend
   chart.legend().useHtml(true);
@@ -215,22 +167,32 @@ function drawChart(data, container, title) {
   const dateTimeTicks = dateTimeScale.ticks();
 
   // get period of time from the first and the last point in hours
-  const periodInHours =
-    (data[0].data[data[0].data.length - 1][0] - data[0].data[0][0]) / 3600000;
-
+  let periodInHours = 1;
+  if (data.length > 0) {
+    periodInHours =
+      (data[0].data[data[0].data.length - 1][0] - data[0].data[0][0]) /
+      (1000 * 60 * 60);
+  }
   // format the x axis ticks
-  if (periodInHours <= 1) {
-    dateTimeTicks.interval("minutes", 5);
-  } else if (periodInHours <= 6) {
-    dateTimeTicks.interval("minutes", 20);
-  } else if (periodInHours <= 12) {
-    dateTimeTicks.interval("hours", 1);
-  } else if (periodInHours <= 24) {
-    dateTimeTicks.interval("hours", 2);
-  } else if (periodInHours <= 240) {
-    dateTimeTicks.interval("days", 1);
-  } else if (periodInHours <= 720) {
-    dateTimeTicks.interval("days", 3);
+  switch (true) {
+    case periodInHours <= 1:
+      dateTimeTicks.interval("minutes", 5);
+      break;
+    case periodInHours <= 6:
+      dateTimeTicks.interval("minutes", 20);
+      break;
+    case periodInHours <= 12:
+      dateTimeTicks.interval("hours", 1);
+      break;
+    case periodInHours <= 24:
+      dateTimeTicks.interval("hours", 2);
+      break;
+    case periodInHours <= 240:
+      dateTimeTicks.interval("days", 1);
+      break;
+    case periodInHours <= 720:
+      dateTimeTicks.interval("days", 3);
+      break;
   }
 
   // apply Date Time scale
@@ -250,4 +212,35 @@ function drawChart(data, container, title) {
 
   // initiate chart display
   chart.draw();
+}
+
+function getReadableDuration(minutes) {
+  let tempMinutes = minutes;
+  let readableDate = "";
+  if (tempMinutes >= 60 * 24) {
+    readableDate += new String(Math.floor(tempMinutes / (60 * 24))) + "d";
+    tempMinutes = tempMinutes % (60 * 24);
+  }
+  if (tempMinutes >= 60) {
+    readableDate += new String(Math.floor(tempMinutes / 60)) + "h";
+    tempMinutes = tempMinutes % 60;
+  }
+  if (tempMinutes > 0) {
+    readableDate += `${tempMinutes}m`;
+  }
+  return readableDate;
+}
+
+function sortFilesOrNodes(arr) {
+  const sortedData = arr.sort((a, b) => {
+    return (
+      b.data.reduce((acc, element) => {
+        return (acc += element[1]);
+      }, 0) -
+      a.data.reduce((acc, element) => {
+        return (acc += element[1]);
+      }, 0)
+    );
+  });
+  return sortedData;
 }
